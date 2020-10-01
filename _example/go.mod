5484bfdf4307d388ba1c078298db55674552f0a6
hironaga <h-hironaga@ifreet.co.jp>
2020-10-01 15:27:44 +0900

    add example

---
+++ _example/go.mod
@@ -0,0 +1,10 @@
+module _example
+
+go 1.15
+
+require (
+	github.com/Hironaga06/gorm-pagination v0.0.0-20201001052708-29f4e64901a6
+	github.com/gin-gonic/gin v1.6.3
+	gorm.io/driver/postgres v1.0.2
+	gorm.io/gorm v1.20.2
+)
---
+++ _example/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	pagination "github.com/Hironaga06/gorm-pagination"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+}
+
+func main() {
+	db, err := gorm.Open(postgres.Open("example.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.AutoMigrate(&User{}); err != nil {
+		panic(err)
+	}
+	var count int64
+	if err := db.Model(User{}).Count(&count).Error; err != nil {
+		panic(err)
+	}
+	if count == 0 {
+		users := []User{
+			User{Name: "a"},
+			User{Name: "b"},
+			User{Name: "c"},
+			User{Name: "d"},
+			User{Name: "e"},
+			User{Name: "f"},
+			User{Name: "g"},
+			User{Name: "h"},
+		}
+		if err := db.Create(&users).Error; err != nil {
+			panic(err)
+		}
+		fmt.Println("insert complite!")
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		page, err := strconv.Atoi(c.DefaultQuery("offset", "1"))
+		if err != nil {
+			panic(err)
+		}
+		limit, err := strconv.Atoi(c.DefaultQuery("limit", "3"))
+		if err != nil {
+			panic(err)
+		}
+		var users []User
+		p := pagination.New(db, 1, 3, []string{"id asc"}, &users, true)
+		result, err := p.Paging()
+		if err != nil {
+			panic(err)
+		}
+		c.JSON(200, result)
+	})
+	r.Run()
+}
